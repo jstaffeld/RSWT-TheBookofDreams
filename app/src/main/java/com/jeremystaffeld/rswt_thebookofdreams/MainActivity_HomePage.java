76af306f58ff5e4421254cf61ba0e25e562eaa70
jstaffeld <ninjaj5@hotmail.com>
2016-12-06 02:37:57 -0600

    Initial commit

---
+++ app/src/main/java/com/jeremystaffeld/rswt_thebookofdreams/MainActivity_HomePage.java
@@ -0,0 +1,13 @@
+package com.jeremystaffeld.rswt_thebookofdreams;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__home_page);
+    }
+}
